cfae8a9892e7d494523fcbb176c05e6df704f908
KathyHan19 <20578233@student.westernsydney.edu.au>
2022-11-30 13:04:05 +1100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>xceeds</title>
+        <title>xceeds 123</title>
     </head>
     <body>
         <nav class="nav">
