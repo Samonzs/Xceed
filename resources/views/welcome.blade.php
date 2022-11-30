6d65466d3bd4d19e5f59cd76c462793c5606d150
wkicback <19756006@student.westernsydney.edu.au>
2022-11-30 11:05:26 +1100

    s

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>xceed</title>
+        <title>xceeds</title>
     </head>
     <body>
         <nav class="nav">
