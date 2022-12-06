a7fd597803e094e5ac4cd7b5add7dfaa4300d5d6
Samonzs <rouhallahali2002@gmail.com>
2022-12-06 13:15:46 +1100

    login css, laravell route fix

--- resources/views/login.php
+++ resources/views/login.php
@@ -2,30 +2,74 @@
 <html lang="en" dir="ltr">
 <head>
     <meta charset="utf-8">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+        *{
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+        body
+        {
+            background: rgb(219, 226, 226);
+        }
+        .row{
+            background: white;
+            border radius: 30px;
+        }
+
+        img{
+            border-top-left-radius: 30px;
+            border-bottom-left-radius: 30px;
+        }
+    </style>
 </head>
 <body>
-    
     <nav>
         <div>
-            <a href="homePage.php">Home Page</a>
+            <a href="{{URL('login')}}">Home Page</a>
         </div>
     </nav>
+    
+    <section class="Form my-4 mx-5">
+        <div class="container">
+            <div class="row no-gutters">
+                
+                <div class="col-lg">
 
-            <label>Login Page</label>
+                    <img src="images/logo.jpg" class="img-fluid" alt="Logo">  
 
-            <section>
-                <form action="login.php" method="post">
-                <h2>LOGIN</h2>
-                    <div>
-                        <label>EMAIL</label>
-                        <input class="inputBox" name="email" type="text" placeholder="Email">
-                    </div>
-                    <div>
-                        <label class = "formLabel">PASSWORD</label>
-                        <input class="inputBox" name ="password" type="password"  placeholder="Password">
-                    </div>
-                    <button name= "submit" type="submit" class="submit">LOGIN</button>
-            </section>
-    </form>
+                </div>
+
+                <div class="col-lg-7">
+                    <form >
+                        <div class= "form-row">
+                            <div class="col-lg-7">
+                                <label>Email</label>
+                                <input class="form-control" name="email" type="text" placeholder="Email Address">
+                            </div>
+                        </div>
+
+                        <div class= "form-row">
+                            <div class="col-lg-7">
+                                <label>Password</label>
+                                <input class="form-control" name ="password" type="password"  placeholder="Password">
+                            </div>
+                        </div>
+
+                        <div class= "form-row">
+                            <div class="col-lg-7">
+                                <button name= "submit" type="button" class="submit">Login</button>      
+                            </div>
+                        </div>
+                        <a href="#">Forgot Password</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>      
 </body>
 </html>
+
