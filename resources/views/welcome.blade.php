3fcb31768bd7855aad49be653b32d4a1e8095b66
completeBeta <85345190+completeBeta@users.noreply.github.com>
2022-12-07 12:32:17 +1100

    dash push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,15 +37,15 @@
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">Quoting <span class="caret"></span></a>
             <ul class="dropdown-menu">
-                <li><a href="#">Create a Quote</a></li>
-                <li><a href="#">List of Quotes</a></li>
+                <li><a href="{{url('createQuotes')}}">Create a Quote</a></li>
+                <li><a href="{{url('listOfQuotes')}}">List of Quotes</a></li>
             </ul>
             <!-- <li><a href="#">section 2</a></li>
             <li><a href="#">Section 3</a></li> -->
         </ul>
         <ul class="nav navbar-nav navbar-right">
             <!-- The glyphicon is just a symbol to use that can be called through the css and js components of bootstrap -->
-            <li><a href="CreateStaff.php"><span class="glyphicon glyphicon-user"></span> Create Staff</a></li>
+            <li><a href="{{url('createStaff')}}"><span class="glyphicon glyphicon-user"></span> Create Staff</a></li>
             <li><a href="{{url('login')}}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
         </ul>
     </div>
