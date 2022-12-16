75db2a2cc9efcbffba2ff1bb19c092efbbd9933f
completeBeta <85345190+completeBeta@users.noreply.github.com>
2022-12-16 16:27:49 +1100

    create variation change push

--- resources/views/createQuotes.php
+++ resources/views/createQuotes.php
@@ -87,28 +87,44 @@
                </style>
             </head>
             <form>
+               <!-- customer dets -->
                <label for="fname">First name:</label><br>
                <input type="text" id="fname" name="fname" value=""><br><br>
                <label for="lname">Last name:</label><br>
                <input type="text" id="lname" name="lname" value=""><br><br>
+               <label for="compName">Company Name:</label><br>
+               <input type="text" id="compName" name="compName" value=""><br><br>
+               <label for="date">Date:</label><br>
+               <input type="text" id="date" name="date" value=""><br><br>
                <label for="pNumber">Phone Number:</label><br>
                <input type="text" id="pNumber" name="pNumber" value=""><br><br>
-               <label for="Address">Address line:</label><br>
-               <input type="text" id="Address" name="Address" value=""><br><br>
+               <label for="abn">ABN:</label><br>
+               <input type="text" id="abn" name="abn" value=""><br><br>
+               <label for="address">Address line:</label><br>
+               <input type="text" id="address" name="address" value=""><br><br>
                <label for="suburb">Suburb:</label><br>
                <input type="text" id="suburb" name="suburb" value=""><br><br>
                <label for="postcode">Post Code:</label><br>
                <input type="text" id="postcode" name="postcode" value=""><br><br>
                <label for="email">Email:</label><br>
                <input type="text" id="email" name="email" value=""><br><br>
-               <label for="email">Job Reference Number</label><br>
+
+               <!-- Variations dets -->
+
+               <label for="jrNumber">Job Reference Number</label><br>
                <input type="number" id="jrNumber" name="jrNumber" value=""><br><br>
+               <label for="ordNumber">Order Number</label><br>
+               <input type="number" id="ordNumber" name="ordNumber" value=""><br><br>
+               <label for="siteName">Site Name:</label><br>
+               <input type="text" id="siteName" name="siteName" value=""><br>
+               <label for="siteAddress">Site Address:</label><br>
+               <input type="text" id="siteAddress" name="siteAddress" value=""><br><br>
                <label for="quotedesc">Description of Quote:</label><br>
                <textarea id="quotedesc" name="quotedesc" rows="10" cols="55"></textarea>
                <br><br>
                
                
-               Variations
+               <!-- Variations (item price inputs) -->
 
                <div class="wrapper">
                   <div id="survey_options">
