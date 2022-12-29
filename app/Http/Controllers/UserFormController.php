3cd89ed099c7dd8a935c2007e9ae56952753f6ff
Samonzs <rouhallahali2002@gmail.com>
2022-12-29 13:42:51 +1100

    databas for staff user creation anmd validation

--- app/Http/Controllers/UserFormController.php
+++ app/Http/Controllers/UserFormController.php
@@ -17,13 +17,12 @@ class UserFormController extends Controller
                 'string',
                 'email',
                 'max:255',
-                'unique:users',
                 'regex:/^\w+[-\.\w]*@(?!(?:outlook|myemail|yahoo)\.com$)\w+[-\.\w]*?\.\w{2,4}$/'
             ],
             'password' => [
                 'required',
                 'string',
-                'min:8',             // must be at least 10 characters in length
+                'min:8',              // must be at least 10 characters in length
                 'regex:/[A-Z]/',      // must contain at least one uppercase letter
                 'regex:/[0-9]/',      // must contain at least one digit
                 'regex:/[@$!%*#?&]/', // must contain a special character  
