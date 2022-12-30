04a6298124e30087cd02b4a7142ee0c02fa06f95
Samonzs <rouhallahali2002@gmail.com>
2022-12-30 15:42:21 +1100

    start on crud for TaS (will finish soon)

---
+++ app/Models/TermsAndConditions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TermsAndConditions extends Model
+{
+    protected $table = 'contacts';
+    protected $fillable = ['Terms and Conditions'];
+
+}
