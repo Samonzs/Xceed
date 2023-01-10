60bead5a92727c6dfcb8c7a53c2e00df298330d8
Samonzs <rouhallahali2002@gmail.com>
2023-01-10 12:44:40 +1100

    admin creation (seeder for tac and admin login)

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'staffUser']);
+    }
+}
