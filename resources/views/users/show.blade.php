6406154154e245087e6679098abaddd4b2e4dabc
completeBeta <85345190+completeBeta@users.noreply.github.com>
2023-01-19 13:01:20 +1100

    fixed all crud layouts and functionality

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,13 +3,13 @@
  
  
 <div class="card">
-  <div class="card-header">Show Details</div>
+  <div class="card-header"><h1>Show Details</h1></div>
     <div class="card-body">
         <div class="card-body">
-            <h5 class="card-title">First Name : {{ $users->staff_fname }}</h5>
-            <h5 class="card-title">Last Name : {{ $users->staff_lname }}</h5>
-            <p class="card-text">Email : {{ $users->staff_email }}</p>
-            <p class="card-text">Password : {{ $users->password }}</p>
+            <p class="card-title"><strong>First Name</strong>: {{ $users->staff_fname }}</p>
+            <p class="card-title"><strong>Last Name</strong>: {{ $users->staff_lname }}</p>
+            <p class="card-text"><strong>Email</strong>: {{ $users->staff_email }}</p>
+            <p class="card-text"><strong>Password</strong>: {{ $users->password }}</p>
             <a href="<?php echo url('/staffListCrud')?>"><button class="btn btn-secondary btn-info btn-sm">Back</button></a>
         </div>
   </div>
