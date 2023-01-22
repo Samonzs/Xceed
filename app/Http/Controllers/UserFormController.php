ec2801f985219a139af61b06f4a314a5e20f90c5
Samonzs <rouhallahali2002@gmail.com>
2023-01-22 12:56:27 +1100

    signature upload

--- app/Http/Controllers/UserFormController.php
+++ app/Http/Controllers/UserFormController.php
@@ -144,45 +144,14 @@ class UserFormController extends Controller
             'variationDescription' => $request->input('variationDescription'),
             'totalCost' => $request->input('totalCost'),
             'variationDateRequest' => $request->input('variationDateRequest'),
-
             'createdBy' => $request->input('createdBy')
 
            ]); 
             return redirect('confirmation');
         }
     }
-    public function getStaffData(Request $request)
-    {
+    
 
- 
-      
-        //validation for staff creation page
-        /*
-        $request->validate([
-            'staff_fname' => 'required|regex:/^([^0-9]*)$/|min:3',
-            'staff_lname' => 'required|regex:/^([^0-9]*)$/|min:3',
-            'staff_email' => [
-                'required',
-                'string',
-                'unique:users',
-                'email',
-                'max:255',
-                'regex:/^\w+[-\.\w]*@(?!(?:outlook|myemail|yahoo)\.com$)\w+[-\.\w]*?\.\w{2,4}$/'
-            ],
-            'password' => [
-                'required',
-                'confirmed',       
-                'string',
-                'min:8',              // must be at least 10 characters in length
-                'regex:/[A-Z]/',      // must contain at least one uppercase letter
-                'regex:/[0-9]/',      // must contain at least one digit
-                'regex:/[@$!%*#?&]/', // must contain a special character  
-            ],
-            'password.regex' => 'The password must contain a capital letter, number, and symbol ']);
-
-        return $request->validate;
-        */
-    }
    
 
 }
