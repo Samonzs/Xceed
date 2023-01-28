1849ad59fd16071eba0d03f6abe52d4941809c2e
Samonzs <rouhallahali2002@gmail.com>
2023-01-28 11:18:43 +1100

    update

--- resources/views/confirmation.blade.php
+++ resources/views/confirmation.blade.php
@@ -266,7 +266,13 @@
                             <input name="id" type="hidden" value="{{$item['id'] }}"></input>
                             <br><br>
                             <button id="clear" class="btn btn-danger">Clear Signature</button>
-                            <button class="btn btn-success">Save</button>
+              
+                            <button id= "Save" name="save" class="btn btn-success" onclick="">Save</button>
+
+                            <script>
+                              
+                            </script>
+
                             <input id="signature" name="signed" style="display: none" value="{{ old('signed') }}"></input>
                         </div>
                     </form>
@@ -275,6 +281,7 @@
        </div>
    </div>
 </div>
+
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> 
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js" ></script>
